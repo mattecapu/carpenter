b91603bb0f59a89977c0d9f809ae108e47095591
Matteo Capucci <mattecapu@live.it>
2014-11-23 12:34:55 +0100

    updated to use assertResourceExists()

--- validateResourceRequest.js
+++ validateResourceRequest.js
@@ -3,20 +3,27 @@
 */
 
 var typs = require('typs');
+var assertResourceExists = require('./assertResourceExists.js');
 
 module.exports = function(resource_request, context) {
-	if (!typs(context.resources[resource_request.resource]).notNull().check()) {
-		throw new jsonError({
-			title: 'Resource not found',
-			detail: '\'' + resource_request.resource + '\' doesn\'t exist',
-			status: 404
+	assertResourceExists(resource_request.resource, context);
+	
+	if (typs(resource_request.ids).notNull().check()) {
+		resource_request.ids.forEach((id) => {
+			if (typs(id).isnt(context.resources[resource_request.resource].type[context.resources[resource_request.resource].keys.primary])) {
+				throw new jsonError({
+					title: 'Bad request',
+					detail: 'One or more ids specificed for the request are not \'' + resource_request.resource + '\' valid ids',
+					status: 400
+				});
+			}
 		});
 	}
 
 	var all_fields_exists = resource_request.resource.fields.every((field) => {
 		return -1 !== Object.keys(context.resources[resource_request.resource].type).indexOf(field);
 	});
-	if(!all_fields_exists) {
+	if (!all_fields_exists) {
 		throw new jsonError({
 			title: 'Unknown fields',
 			detail: 'One or more fields requested don\'t belong to \'' + resource_request.resource + '\' resource',
@@ -27,7 +34,7 @@ module.exports = function(resource_request, context) {
 	var all_filters_exists = resource_request.resource.filters.every(function({field, values}) {
 		return -1 !== Object.keys(context.resources[resource_request.resource].type).indexOf(field);
 	});
-	if(!all_filters_exists) {
+	if (!all_filters_exists) {
 		throw new jsonError({
 			title: 'Unknown fields',
 			detail: 'One or more fields in the requested filters don\'t belong to \'' + resource_request.resource + '\' resource',
@@ -38,7 +45,7 @@ module.exports = function(resource_request, context) {
 	var all_sorters_exists = resource_request.resource.sorters.every(function({field, asc}) {
 		return -1 !== Object.keys(context.resources[resource_request.resource].type).indexOf(field);
 	});
-	if(!all_sorters_exists) {
+	if (!all_sorters_exists) {
 		throw new jsonError({
 			title: 'Unknown fields',
 			detail: 'One or more fields in the requested sorters don\'t belong to \'' + resource_request.resource + '\' resource',
