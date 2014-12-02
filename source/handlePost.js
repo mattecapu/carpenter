fbd82a49898209675e34ba25e522e9307eb25188
Matteo Capucci <mattecapu@live.it>
2014-12-02 22:30:08 +0100

    fixed support for multiple insertions and relative per-insertion error handling

--- source/handlePost.js
+++ source/handlePost.js
@@ -16,14 +16,14 @@ var handlePost = function (request, body, context) {
 	var objects = body[request.primary.resource];
 	objects = typs(objects).array().check()	? objects : [objects];
 
-	var query = squel.insert().into(context.resources[request.primary.resource].sql_table);
+	return Promise.map(objects, (object) => {
+		// prepare the INSERT query
+		var query = squel.insert().into(context.resources[request.primary.resource].sql_table);
 
-	// validate the resource objects
-	objects.forEach((data) => {
 		// typecheck all the fields
 		Object.keys(context.resources[request.primary.resource].structure).forEach((field) => {
 			if (field === context.resources[request.primary.resource].keys.primary) return;
-			if (typs(data[field]).isnt(context.resources[request.primary.resource].structure[field].type)) {
+			if (typs(object[field]).isnt(context.resources[request.primary.resource].structure[field].type)) {
 				throw new jsonError({
 					title: 'Bad request',
 					detail: 'The value specified for the field \'' + field + '\' doesn\'t typecheck',
@@ -31,26 +31,65 @@ var handlePost = function (request, body, context) {
 				});
 			}
 		});
-	})
 
-	var rows = objects.map((data) => {
 		// build the row object
 		var row = {};
-		Object.keys(data).forEach((field) => {
-			row[context.resources[request.primary.resource].structure[field].sql_column] = data[field];
+		Object.keys(object).forEach((field) => {
+			row[context.resources[request.primary.resource].structure[field].sql_column] = object[field];
 		});
-		return row;
-	});
-
-	query = query.setFieldsRows(rows);
-
-	return context.callQuery(query).spread((stats) => {
+		query = query.setFields(row);
+console.log(query.toString());
+		return query;
+	}).map((query) => context.callQuery(query).reflect()).all().then((stats) => {
+		console.log('fulfilled',stats.map((s) => s.isFulfilled()));
+		console.log('rejected',stats.map((s) => s.isRejected()));
 		var handleGet = require('./handleGet.js');
 		var parseUrl = require('./parseUrl.js');
-		return handleGet(parseUrl('/' + request.primary.resource + '/' + [stats.insertId].join(','), context), null, context);
-	}).then((response) => {
-		response.status = 201;
-		return response;
+
+		var ids = stats.filter((stat) => stat.isFulfilled()).map((stat) => stat.value()[0].insertId);
+		var errors = stats.map((stat, i) => {
+			if (!stat.isRejected()) return null;
+			var err = stat.reason();
+			err.object.title = '(On request #' + (i + 1) + ') ' + err.object.title;
+			return err;
+		}).filter((error) => null !== error);
+
+		console.log('ids',ids);
+		console.log('errors',errors);
+
+		var promise_result = null;
+
+		// if some resource have been actually created
+		if (0 !== ids.length) {
+			// URL to the new created resources
+			var location = '/' + request.primary.resource + '/' + ids.join(',');
+
+			// immediatly return the documents created (as the spec says)
+			promise_result = handleGet(
+				parseUrl(location, context),
+				null, context
+			).then((response) => {
+				response.location = location;
+				response.status = 201;
+				return response;
+			});
+		} else {
+			// no resources created, that's a failure
+			promise_result = Promise.resolve({response: {}, status: 400});
+		}
+
+		// if there are some errors
+		if(0 !== errors.length) {
+			promise_result = promise_result.then((response) => {
+				if (0 !== errors.length) {
+					response.response.errors = errors;
+				}
+				return response;
+			});
+		}
+
+		// return the response
+		return promise_result;
 	});
 };
 
