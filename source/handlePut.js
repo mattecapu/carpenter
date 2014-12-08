0486892c78af61665da3fbed869e83877e23871d
Matteo Capucci <mattecapu@live.it>
2014-12-08 12:06:31 +0100

    Enhanced PUT requests handling * support for linked resources * removed squel-mocking thanks to the improvements in Carpenter.callQuery * added check for actual need to have IDs, useful for linked resources

--- source/handlePut.js
+++ source/handlePut.js
@@ -8,6 +8,7 @@ var squel = require('squel');
 var Promise = require('bluebird');
 
 var jsonError = require('./jsonError.js');
+var {filterBy, selectBy} = require('./queryBuilder.js');
 
 
 var handlePut = function (request, body, context) {
@@ -16,30 +17,45 @@ var handlePut = function (request, body, context) {
 	var objects = body[request.primary.resource];
 	objects = typs(objects).array().check()	? objects : [objects];
 
-	if (request.primary.ids.length !== objects.length) {
-		throw new jsonError({
-			title: 'Bad request',
-			detail: 'All the IDs of the resources to update must be specified in the request URL',
-			status: 400
-		});
-	}
+	return context.callQuery(
+		selectBy(request.primary, context)
+	).spread((rows) => rows.length > 1).then((check_ids) => {
+		// if the two differ, something is missing
+		request.primary.ids = request.primary.ids || [];
+		if (check_ids && request.primary.ids.length !== objects.length) {
+			throw new jsonError({
+				title: 'Bad request',
+				detail: 'All the IDs of the resources to update must be specified both in the URL and in the body',
+				status: 400
+			});
+		}
 
-	return Promise.map(objects, (object, i) => {
-		// prepare the INSERT query
-		var query = squel.update().table(context.resources[request.primary.resource].sql_table);
+		return Promise.map(objects, (object, i) => {
+			// prepare the INSERT query
+			var query = squel.update().table(context.resources[request.primary.resource].sql_table);
+			var new_fields = Object.keys(object);
+			var primary_key_field = context.resources[request.primary.resource].keys.primary;
 
-		// typecheck all the fields
-		Object.keys(object).forEach((field) => {
-			if (typs(object[field]).isnt(context.resources[request.primary.resource].structure[field].type)) {
-				throw new jsonError({
-					title: 'Bad request',
-					detail: 'The value specified for the field \'' + field + '\' doesn\'t typecheck',
-					status: 400
-				});
-			}
+			// typecheck all the fields
+			new_fields.forEach((field) => {
+				if (typs(object[field]).isnt(context.resources[request.primary.resource].structure[field].type)) {
+					throw new jsonError({
+						title: 'Bad request',
+						detail: 'The value specified for the field \'' + field + '\' doesn\'t typecheck',
+						status: 400
+					});
+				}
+			});
 
-			if (field === context.resources[request.primary.resource].keys.primary) {
-				if (-1 === request.primary.ids.indexOf(object[field])) {
+			if (check_ids) {
+				// check if we have all the necessary IDs
+				if (typs(object[primary_key_field]).Null().check()) {
+					throw new jsonError({
+						title: 'Bad request',
+						detail: 'The ID of resource #' + (i + 1) + ' must be specified',
+						status: 400
+					});
+				} else if (-1 === request.primary.ids.indexOf(object[primary_key_field])) {
 					throw new jsonError({
 						title: 'Bad request',
 						detail: 'The ID of resource #' + (i + 1) + ' must be specified in the request URL',
@@ -47,37 +63,37 @@ var handlePut = function (request, body, context) {
 					});
 				}
 			}
-		});
 
-		// build the row object
-		var row = {};
-		Object.keys(object).forEach((field) => {
-			if (field === context.resources[request.primary.resource].keys.primary) return;
-			row[context.resources[request.primary.resource].structure[field].sql_column] = object[field];
-		});
-		query = query.setFields(row).where(
-			context.resources[request.primary.resource].keys.primary + ' = ?',
-			object[context.resources[request.primary.resource].keys.primary]
-		);
+			// build the row object
+			var row = {};
+			new_fields.filter((f) => f !== primary_key_field).forEach((field) => {
+				row[context.resources[request.primary.resource].structure[field].sql_column] = object[field];
+			});
+			if (typs(row).notEmpty().doesntCheck()) {
+				return null;
+			} else {
+				// we use filterBy to support complex requests
+				query = filterBy(query.setFields(row), request.primary, context);
 
-		return query;
+				// filter by ID if it's needed
+				if (check_ids) {
+					query = query.where(primary_key_field + ' = ?', object[primary_key_field]);
+				}
 
-	}).then((queries) => {
-		// simulates a squel query
-		context.callQuery({
-			toParam: () => {
-				return {text: 'START TRANSACTION', values: []};
+				return query;
 			}
 		});
+	}).catch((x) => {
+		throw x;
+	}).then((queries) => {
+		context.callQuery('START TRANSACTION');
 		return queries;
 	}).map((query) => context.callQuery(query)).all().then((stats) => {
-		// simulates a squel query
-		context.callQuery({
-			toParam: () => {
-				return {text: 'COMMIT', values: []};
-			}
-		});
+		context.callQuery('COMMIT');
 		return {response: {}, status: 204};
+	}).catch((error) => {
+		context.callQuery('ROLLBACK');
+		throw error;
 	});
 };
 
