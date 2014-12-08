5e7bf36c7bb0d0d435686eef8767a0fe40c41606
Matteo Capucci <mattecapu@live.it>
2014-12-08 12:12:37 +0100

    minor improvements to jsonError object building

--- source/jsonError.js
+++ source/jsonError.js
@@ -4,16 +4,19 @@
 
 
 var jsonError = function ({title, detail, status, code, href, links, path, id}) {
+	// essential info
 	this.object = {
 		title: title || 'Error',
 		detail: detail || 'A unspecified error was fired by the application',
-		status: status || 400,
-		code: code,
-		href: href,
-		links: links,
-		path: path,
-		id: id
-	}
+		status: status || 400
+	};
+
+	// optional info: let's not pollute the error object with empty fields
+	if (code) object.code = code;
+	if (href) object.href = href;
+	if (links) object.links = links;
+	if (path) object.path = path;
+	if (id) object.id = id;
 };
 
 module.exports = jsonError;
