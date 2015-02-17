4644e7e343c0ea5c9ff9bea0fd1caffe26513036
Matteo Capucci <mattecapu@live.it>
2015-02-17 20:47:42 +0100

    changed 'type' to 'domain', to disambiguate for the new specification

--- source/domains.js
+++ source/domains.js
@@ -1,18 +1,19 @@
 /*!
-	Types
-	collection of common types
+	Domains
+	collection of common attribute types
 */
 
 var typs = require('typs');
 
-var types = {
+var domains = {
 	id: typs().integer().positive().notZero(),
 	email: typs().string().regex(/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b/i),
 	flag: typs().matchAny([typs().equals(0), typs().equals(1), typs().bool()]),
 	text: typs().string(),
 	string: typs().string(),
+	timestamp: typs().matchesAny([typs().Null(), typs().positive()]),
 
 	custom: typs()
 };
 
-module.exports = types;
+module.exports = domains;
