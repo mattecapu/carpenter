32807e8e117af86570e8a08057d4ec7f88f763d4
Matteo Capucci <mattecapu@live.it>
2015-02-22 14:00:08 +0100

    modified flag type

--- source/domains.js
+++ source/domains.js
@@ -8,7 +8,7 @@ var typs = require('typs');
 var domains = {
 	id: typs().integer().positive().notZero(),
 	email: typs().string().regex(/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b/i),
-	flag: typs().matchAny([typs().equals(0), typs().equals(1), typs().bool()]),
+	flag: typs().matchesAny([typs().oneOf([0, 1]), typs().bool()]),
 	text: typs().string(),
 	string: typs().string(),
 	timestamp: typs().matchesAny([typs().Null(), typs().positive()]),
