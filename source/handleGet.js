d63f158d6379a971eb5a355fef08858e439c94d7
Matteo Capucci <mattecapu@live.it>
2015-03-01 10:34:36 +0100

    partial rewrote to use the new simplified format

--- source/handleGet.js
+++ source/handleGet.js
@@ -5,6 +5,7 @@
 
 var squel = require('squel');
 var Promise = require('bluebird');
+var typs = require('typs');
 
 var {filterBy, selectBy} = require('./queryBuilder.js');
 
@@ -15,36 +16,64 @@ var handleGet = function (request, body, context) {
 	let primary_query = selectBy(request.primary, context);
 	let linked_queries = request.linked.map((linked) => selectBy(linked, context));
 
-	var promises = [].concat(primary_query, linked_queries).map((query) => context.callQuery(query));
+	let promises = [].concat(primary_query, linked_queries).map((query) => context.callQuery(query));
+
+	let normalize_response = (request, response) => {
+		// walk the request searching for "possibility of collection"
+		// if all the parent resources are necessarily single, then we return a single object
+		// otherwise, if even just one parent resource can be a collection, return a collection
+		let is_single = true;
+		let parent_resource = request;
+		do {
+			// it can be a single resource only if:
+			// 1. is related to the parent resource with a one-to-one relationship (i.e. /articles/2/author)
+			// 2. it has been requested with a single ID (i.e. /articles/2)
+			is_single = parent_resource.relationship && parent_resource.relationship.to === 'one'
+						|| parent_resource.ids && (parent_resource.ids.length === 1 && parent_resource.ids[0] !== 'any');
+			parent_resource = parent_resource.superset;
+		} while (is_single && typs(parent_resource).def().check());
+
+		if (is_single) {
+			// single object, empty response
+			if (typs(response[0]).undef().check()) {
+				return null;
+			}
+			// single object, defined
+			return response[0];
+		} else {
+			// collection, empty response
+			if (typs(response).undef().check()) {
+				return [];
+			}
+			// populated collection
+			return response;
+		}
+	};
 
 	return Promise.all(promises).map((res) => res[0]).then((results) => {
 
 		let response = {};
-
-		// empty response
-		if (0 === results[0].length) {
-			return {response, status: 404};
-		}
+		let status = results[0].length > 0 ? 200 : 404;
 
 		// primary resource
-		response.data = results[0];
+		response[request.primary.type] = results[0];
 		// add type member
-		response.data.forEach((result) => result.type = request.primary.type);
-		// if its a single object, unwrap it from the array
-		if (response.data.length === 1) {
-			response.data = response.data[0];
-		}
+		response[request.primary.type].forEach((result) => result.type = request.primary.type);
+		// if a single object was requested, return it as an object and not as a collection
+		response[request.primary.type] = normalize_response(request.primary, response[request.primary.type]);
+
 
 		// are there any additional resources?
 		if (request.linked.length) {
-			response.linked = {};
-			
+			// skip the first resource (primary)
 			results.slice(1).forEach((result, i) => {
-				response.linked[request.linked[i].type] = result;
+				response[request.linked[i].relationship.name] = result;
+				// if it's a single resource, return it as an object
+				response[request.linked[i].relationship.name] = normalize_response(request.linked[i], response[request.linked[i].relationship.name]);
 			});
 		}
 
-		return {response, status: 200};
+		return {response, status};
 	});
 };
 
