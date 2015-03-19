b5b8283a37631b87279fdc35c4aba533787d3ffd
Matteo Capucci <mattecapu@live.it>
2015-03-19 19:57:45 +0100

    sorta support the new queryBuilder, but it's just an wibbly woobly timey wimey stuff

--- source/handleGet.js
+++ source/handleGet.js
@@ -13,17 +13,13 @@ var {filterBy, selectBy} = require('./queryBuilder.js');
 var handleGet = function (request, body, context) {
 	squel.useFlavour('mysql');
 
-	let main = selectBy(request, context);
-	let related = request.related.map((rel) => selectBy(rel, context));
+	let query = selectBy(request, context);
 
-	let query_promises = [].concat(main, related).map((query) => context.callQuery(query));
-
-	let normalize_response = (request, response) => {
+	let normalize_response = (parent_resource, response) => {
 		// walk the request searching for "possibility of collection"
 		// if all the parent resources are necessarily single, then we return a single object
 		// otherwise, if even just one parent resource can be a collection, return a collection
 		let is_single = true;
-		let parent_resource = request;
 		do {
 			// it can be a single resource only if:
 			// 1. is related to the parent resource with a one-to-one relationship (i.e. /articles/2/author)
@@ -50,17 +46,17 @@ var handleGet = function (request, body, context) {
 		}
 	};
 
-	return Promise.all(query_promises).map((res) => res[0]).then((results) => {
+	return context.callQuery(query).then(([results]) => {
 
 		let response = {};
-		let status = results[0].length > 0 ? 200 : 404;
+		let status = results.length > 0 ? 200 : 404;
 
 		// main resource
-		response[request.type] = results[0];
+		response[request.main.type] = results;
 		// add type member
-		response[request.type].forEach((result) => result.type = request.type);
+		response[request.main.type].forEach((result) => result.type = request.main.type);
 		// if a single object was requested, return it as an object and not as a collection
-		response[request.type] = normalize_response(request, response[request.type]);
+		response[request.main.type] = normalize_response(request, response[request.main.type]);
 
 
 		// are there any additional resources?
