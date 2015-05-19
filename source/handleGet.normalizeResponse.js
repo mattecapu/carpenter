ba69601a21a6bc53f3b0d5886aab5dfac2292ed3
Matteo Capucci <mattecapu@live.it>
2015-05-19 22:41:38 +0200

    check for other filters in addition to IDs

--- source/handleGet.normalizeResponse.js
+++ source/handleGet.normalizeResponse.js
@@ -15,7 +15,7 @@ export default function (request, response) {
 		// it *can* be a single resource only if:
 		// 1. it has been requested with a single ID (i.e. /articles/2)
 		// 2. it's related to the parent resource with a one-to-one relationship (i.e. /articles/2/author)
-		is_single = parent_resource.ids.length === 1
+		is_single = (parent_resource.ids.length === 1 && parent_resource.filters.length === 0)
 					|| parent_resource.relationship && parent_resource.relationship.to === 'one';
 		parent_resource = parent_resource.superset;
 	} while (is_single && typs(parent_resource).def().check());
